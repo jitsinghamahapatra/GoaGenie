fe9b61475d084f700dd9a8a64a15dc999e615709
jitsinghamahapatra <jitsinghamahapatra2006@gmail.com>
2026-04-21 18:42:48 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -13,8 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
-  methods: ['GET', 'POST'],
+  origin: [
+    "http://localhost:5173",
+    "https://goa-genie.vercel.app"
+  ],
+  methods: ["GET", "POST"],
   credentials: true
 }));
 app.use(express.json());
